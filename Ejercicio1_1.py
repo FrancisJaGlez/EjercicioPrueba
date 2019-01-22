01a9621fc43f0e2f9030a41fe7f67bb69daec489
FrancisJaGlez <46821629+FrancisJaGlez@users.noreply.github.com>
2019-01-22 11:05:19 -0600

    Francisco Javier González Molina, Grupo 03
    :)

---
+++ Ejercicio1_1.py
@@ -0,0 +1,6 @@
+#Francisco Javier González Molina   A01748636
+#Velocidad Promedio
+d=int (input("Distancia recorrida en metros: "))
+t=int(input("Tiempo de viaje en segundos: "))
+vp=d/t
+print ("La velocidad promedio es",vp,"m/s")
